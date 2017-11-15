8dab4a0afff4fa02faef498218e328cf69ef1cbb
Johan sundstrom <johansundstrom@users.noreply.github.com>
2017-11-15 17:32:29 +0100

    Add files via upload
    js

---
+++ js/javascript.js
@@ -0,0 +1,56 @@
+function runme() {
+    // let a = 'johan';
+    // var b = 123;
+    // let c = 456;
+
+    // let d = b + c;
+    // //let e = d.toString();
+    // console.log(a + d.toString());
+
+    let array = [23, 54, 21, 8, 12, 42];
+    console.log(array);
+    console.log(array[1]);
+    console.log(array.sort());
+
+    console.log(array.length);
+    console.log(array.reverse());
+
+    //var car = {owner:"johan", bil1{ [name: "Volvo", color: "svart", year: 2013], bil2:{name: "SAAB", color: "vit"}}};
+    var person = {
+        "owner": "johan", 
+        "age": 57, 
+        "cars": [
+            {type: "Volvo", color: "Svart", year: 1998}, 
+            {type: "Corvette", color: "röd"}, 
+            {type: "BMW"}
+        ]
+    };
+
+
+    console.log(person.cars[1].color + " " + person.cars[1].type);
+ 
+    for (
+    i = 0; i < array.length; i++){
+        //console.log(person.cars[i].type);
+    };
+
+    let text = '{"owner": "johan", "age": 57, "cars": [{"type": "Volvo", "color": "Svart", "year": 1998}, {"type": "Corvette", "color": "röd"}, {"type": "BMW"}]}';
+    var obj = JSON.parse(text); 
+    console.log(obj.cars[1].color);
+
+    myFunction(23, 45);
+
+    function myFunction(a, b) {
+        var sum = a + b;
+        console.log(sum);
+        return sum;
+    }
+
+
+
+}
+
+function clicked(){
+    document.getElementById("demo").innerHTML = "Clicked";
+    console.log("clicked")
+};
